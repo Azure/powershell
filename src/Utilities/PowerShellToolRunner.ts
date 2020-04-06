b9a813ddc8f87f1fbc45813f192687ab8987087f
Akshaya M <aksm@microsoft.com>
2020-04-06 16:15:18 +0530

    added args in PowerShellToolRunner

--- src/Utilities/PowerShellToolRunner.ts
+++ src/Utilities/PowerShellToolRunner.ts
@@ -3,6 +3,7 @@ import * as exec from '@actions/exec';
 
 export default class PowerShellToolRunner {
     static psPath: string;
+    static args: string[] = ['-NoLogo', '-NoProfile', '-NonInteractive'];
 
     static async init() {
         if(!PowerShellToolRunner.psPath) {
@@ -11,11 +12,13 @@ export default class PowerShellToolRunner {
     }
 
     static async executePowerShellCommand(command: string, options: any = {}) {
-        await exec.exec(`${PowerShellToolRunner.psPath} -Command ${command}`, [], options);
+        await exec.exec(`${PowerShellToolRunner.psPath} -Command ${command}`, PowerShellToolRunner.args, options);
     }
 
     static async executePowerShellScriptBlock(scriptBlock: string, options: any = {}): Promise<number> {
-        const exitCode: number = await exec.exec(`${PowerShellToolRunner.psPath} -Command`, [scriptBlock], options)
+        PowerShellToolRunner.args.push(scriptBlock);
+        const exitCode: number = await exec.exec(`${PowerShellToolRunner.psPath} -Command`,
+                     PowerShellToolRunner.args, options);
         return exitCode;
     }
 }
