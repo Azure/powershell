2137350c3f421b1babcdd1677f12a36876705dd0
Akshaya M <aksm@microsoft.com>
2020-04-08 12:49:51 +0530

    added unit tests

---
+++ __tests__/ScriptRunner.test.ts
@@ -0,0 +1,27 @@
+import ScriptRunner from '../src/ScriptRunner';
+
+jest.mock('../src/Utilities/FileUtils');
+jest.mock('../src/Utilities/PowerShellToolRunner');
+jest.mock('../src/Utilities/ScriptBuilder');
+let scriptRunner: ScriptRunner;
+
+beforeAll(() => {
+    scriptRunner = new ScriptRunner("inlineScript", "Stop", true);
+});
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+describe('Testing ScriptRunner', () => {
+    let executeFileSpy;
+    beforeEach(() => {
+        executeFileSpy = jest.spyOn(scriptRunner, 'executeFile');
+    });
+    test('executeFile should pass', async () => {
+        executeFileSpy.mockImplementationOnce(() => Promise.resolve(
+            console.log('Script execution complete')));
+        await scriptRunner.executeFile();
+        expect(executeFileSpy).toHaveBeenCalled();
+    });
+});
