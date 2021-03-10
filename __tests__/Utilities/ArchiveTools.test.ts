6d832b6b394eaf5f6b4b9c18d3e6d0272a4d852c
Amit Kumar <amit-avit@users.noreply.github.com>
2021-03-10 23:48:15 +0000

    Added UTs and fixed existing one

---
+++ __tests__/Utilities/ArchiveTools.test.ts
@@ -0,0 +1,20 @@
+import { ArchiveTools } from "../../src/Utilities/ArchiveTools";
+
+describe('Testing ArchiveTools', () => {
+    test('unzip using powershell to extract', async () => {
+        const archiveTool = new ArchiveTools();
+        archiveTool["unzipUsingPowerShell"] = jest.fn();
+        archiveTool["unzipUsing7Zip"] = jest.fn();
+        await archiveTool.unzip("/usr/share/az_1.1.1.zip", "/usr/share");
+        expect(archiveTool["unzipUsingPowerShell"]).toHaveBeenCalledTimes(1);
+        expect(archiveTool["unzipUsing7Zip"]).not.toHaveBeenCalled();
+    });
+    test('unzip using 7zip to extract', async () => {
+        const archiveTool = new ArchiveTools(true);
+        archiveTool["unzipUsingPowerShell"] = jest.fn();
+        archiveTool["unzipUsing7Zip"] = jest.fn();
+        await archiveTool.unzip("/usr/share/az_1.1.1.zip", "/usr/share");
+        expect(archiveTool["unzipUsingPowerShell"]).not.toHaveBeenCalled();
+        expect(archiveTool["unzipUsing7Zip"]).toHaveBeenCalledTimes(1);
+    });
+});
